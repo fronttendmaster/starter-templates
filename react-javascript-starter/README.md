493fc305a2feeacd37641e29f5eb2d7e98ae5ae6
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-08-31 23:33:03 +0530

    basic application structure setup done

--- react-javascript-starter/README.md
+++ react-javascript-starter/README.md
@@ -6,3 +6,9 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Prime React UI Library Setup
+
+This template uses Prime React UI Library for building UI components.
+
+`npm i primeflex primeicons primereact`
--- react-javascript-starter/src/App.jsx
+++ react-javascript-starter/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css'; // flex
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <nav>
+          <ul className='m-0 p-0 flex gap-4 align-items-center justify-content-center pt-4 '>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>About</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div className='hero-content text-center'>
+          <h1>Javascript + React</h1>
+          <p className='read-the-docs'>
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
+      </main>
+      <footer>
+        <p className='text-center'>&copy; 2024 My React App</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
