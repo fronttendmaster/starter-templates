599924232a294a64a960ccfb8d45845f9bcd54d9
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-08-31 23:42:33 +0530

    common prime module configured

---
+++ react-javascript-starter/common/index.jsx
@@ -0,0 +1,6 @@
+import { Button } from 'primereact/button';
+import { Chips } from 'primereact/chips';
+import { InputText } from 'primereact/inputtext';
+import { Rating } from 'primereact/rating';
+
+export { Button, Chips, InputText, Rating };
--- react-javascript-starter/src/App.jsx
+++ react-javascript-starter/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/themes/lara-light-cyan/theme.css'; //theme
 import 'primereact/resources/primereact.min.css'; //core css
 import 'primeicons/primeicons.css'; //icons
 import 'primeflex/primeflex.css'; // flex
 import './App.css';
+
+import { Rating } from '../common/index';
 
 function App() {
   return (
@@ -21,12 +23,21 @@ function App() {
         </nav>
       </header>
       <main>
-        <div className='hero-content text-center'>
-          <h1>Javascript + React</h1>
-          <p className='read-the-docs'>
-            Click on the Vite and React logos to learn more
-          </p>
-        </div>
+        <section>
+          <article>
+            <div className='hero-content text-center'>
+              <h1>Javascript + React</h1>
+              <p className='read-the-docs'>
+                Click on the Vite and React logos to learn more
+              </p>
+            </div>
+          </article>
+          <article>
+            <div className='card flex justify-content-center'>
+              <Rating value={5} readOnly cancel={false} />
+            </div>
+          </article>
+        </section>
       </main>
       <footer>
         <p className='text-center'>&copy; 2024 My React App</p>
