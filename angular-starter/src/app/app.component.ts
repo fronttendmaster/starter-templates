c7b237e48b210d999b3fd40e90239bf050e154cc
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-09-01 08:40:28 +0530

    primeng ui configuration done

--- angular-starter/src/app/app.component.ts
+++ angular-starter/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimengConfigModule } from './primeng-config/primeng-config.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrimengConfigModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-starter';
